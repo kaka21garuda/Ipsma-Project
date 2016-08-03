04d77d513df6b96b829b4a6de0a4aa409f8ff3ec
Buka Cakrawala <kaka10garuda@gmail.com>
2016-08-02 23:45:56 -0700

    initializing GeoLocation by Using MapKit and CoreLocation:
    *Setting up locationManager
    *Delegating mapView and CLLocationManager
    *Setting up the user's authorization

--- Ipsma3/ViewController.swift
+++ Ipsma3/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //1.. CREATE LOCATION MANAGER WHICH WILL DETECT THE USER'S CURRENT LOCATION
+    let locationManager = CLLocationManager()
+    
+    //SHOWING ALERT WHEN USER DENIED THE AUTHORIZATION
+    let showAlert = UIAlertController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //2..   SETUP LOCATION MANAGER
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        
+        //3.. SETUP MAPVIEW
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .Follow
+        
+        //.. SETUP TEST DATA
+        setupData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        //1.. WHEN STATUS IS NOT DETERMINED
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        //2.. WHEN AUTHORIZATION WERE DENIED, SHOW ALERT!!!
+        else if CLLocationManager.authorizationStatus() == .Denied {
+            showAlert.message = "Location services were previously denied. Please enable location services for this app in Settings."
+        }
+        
+        //EVERYTHING IS SET AND READY TO UPDATE USER'S CURRENT LOCATION!!!
+        else if CLLocationManager.authorizationStatus() == .AuthorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    
+    }
+    
+    func setupData() {
+        //MONITORING REGION
+        
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
