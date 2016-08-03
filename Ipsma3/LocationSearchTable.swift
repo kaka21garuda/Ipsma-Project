724bc7245f66f0e764b93b0146bd2b9fc1c98718
Buka Cakrawala <kaka10garuda@gmail.com>
2016-08-03 12:50:07 -0700

    Table View Data Source

--- Ipsma3/LocationSearchTable.swift
+++ Ipsma3/LocationSearchTable.swift
@@ -22,23 +22,37 @@ class LocationSearchTable: UITableViewController {
 
 extension LocationSearchTable: UISearchResultsUpdating {
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        func updateSearchResultsForSearchController(searchController: UISearchController) {
-            guard let mapView = mapView,
-                let searchBarText = searchController.searchBar.text else { return }
-            let request = MKLocalSearchRequest()
-            request.naturalLanguageQuery = searchBarText
-            request.region = mapView.region
-            let search = MKLocalSearch(request: request)
-            search.startWithCompletionHandler { response, _ in
-                guard let response = response else {
-                    return
-                }
-                self.matchingItems = response.mapItems
-                self.tableView.reloadData()
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { response, _ in
+            guard let response = response else {
+                return
             }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
         }
-
     }
+}
+
+
+
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
+   
 }
 
 
