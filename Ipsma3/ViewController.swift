f39e63299a5dcc72686b35f5acb32823022cf4fa
Buka Cakrawala <kaka10garuda@gmail.com>
2016-08-05 12:26:41 -0700

    RightCallout button was added

--- Ipsma3/ViewController.swift
+++ Ipsma3/ViewController.swift
@@ -16,6 +16,7 @@ protocol HandleMapSearch {
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    
     @IBOutlet weak var mapView: MKMapView!
     
     //PROPERTY FOR UISearchController
@@ -45,8 +46,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .Follow
         
-        //.. SETUP TEST DATA
-        setupData()
+         
         
         let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
         resultSearchController = UISearchController(searchResultsController: locationSearchTable)
@@ -88,34 +88,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
 /****************************************************************************/
     
-    func setupData() {
-        //MONITORING REGION (IF MONITORING REGION IS SUPPORTED ON USER'S DEVICE OR NOT)
-        if CLLocationManager.isMonitoringAvailableForClass(CLCircularRegion.self) {
-            
-            //REGION DATA
-            let title =  "City Hall"
-            let coordinate = CLLocationCoordinate2DMake(37.779398, -122.418706)
-            let regionRadius = 500.0
-            
-            //SETTING UP THE REGION
-            let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude), radius: regionRadius, identifier: title)
-            locationManager.startMonitoringForRegion(region)
-            
-            //ANNOTATION
-            let cityHallAnnotation = MKPointAnnotation()
-            cityHallAnnotation.coordinate = coordinate
-            cityHallAnnotation.title = "\(title)"
-            mapView.addAnnotation(cityHallAnnotation)
-            
-            //PUTTING THE CIRCLE AROUND THE ANNOTATION
-            let circle = MKCircle(centerCoordinate: coordinate, radius: regionRadius)
-            mapView.addOverlay(circle)
-        }
-        
-        else {
-            print("system can't track region")
-        }
-    }
     
     // MKMapViewDelegate's METHOD IN ORDER TO DRAW THE CIRCLE
     func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
@@ -123,6 +95,35 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         circleRenderer.strokeColor = UIColor.redColor()
         circleRenderer.lineWidth = 1.0
         return circleRenderer
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        print("viewForannotation")
+        if annotation is MKUserLocation {
+            //return nil
+            return nil
+        }
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            //println("Pinview was nil")
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.animatesDrop = true
+        }
+        
+        
+        let button = UIButton(type: .ContactAdd)
+        
+        
+        
+        pinView?.rightCalloutAccessoryView = button
+        
+        
+        return pinView
     }
 
 }
