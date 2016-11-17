c7fc3e665c78cbcd2cdc7573493f2670b6b5979b
Buka Cakrawala <kaka10garuda@gmail.com>
2016-11-16 21:58:50 -0800

    Enable to track the user's location

--- Ipsma/LocationSearchViewController.swift
+++ Ipsma/LocationSearchViewController.swift
@@ -10,12 +10,37 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class LocationSearchViewController: UIViewController, CLLocationManagerDelegate {
+class LocationSearchViewController: UIViewController {
     let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
-        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //Triggers the location permission dialog.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+}
+
+extension LocationSearchViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    //This function calls back when the information of the location comes back.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    //to check if there is an error
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
